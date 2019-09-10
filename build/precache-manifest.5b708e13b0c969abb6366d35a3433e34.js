119e4625a6abaf4f7427784edbaf0012847a5ddb
Alain Tran <alaint77@gmail.com>
2019-09-10 16:29:41 +0200

    updated baseUrl to match backend

---
+++ build/precache-manifest.5b708e13b0c969abb6366d35a3433e34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c675efd8e2e9d2f34f381b45972ab689",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fafb88ed1b9c4d5b02f4",
+    "url": "/static/css/main.bec1dcbd.chunk.css"
+  },
+  {
+    "revision": "c92ecfaa8d3c9c06d282",
+    "url": "/static/js/2.6bcb67b5.chunk.js"
+  },
+  {
+    "revision": "fafb88ed1b9c4d5b02f4",
+    "url": "/static/js/main.c8cfad2d.chunk.js"
+  },
+  {
+    "revision": "643b1496a8e20922303c",
+    "url": "/static/js/runtime~main.bf59cc56.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dc3d87911844d81a9619b5aace723b7f.js"
+  "/precache-manifest.5b708e13b0c969abb6366d35a3433e34.js"
 );
 
 self.addEventListener('message', (event) => {
